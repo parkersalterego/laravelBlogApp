d167481d6940370569f29ef7723c43440d0c2a59
parkersalterego <parkersalterego92@gmail.com>
2018-05-14 20:44:23 -0700

    user and post info linked in order to display information accordingly

--- app/Post.php
+++ app/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     public $primaryKey = 'id';
     // TIMESTAMPS
     public $timestamps = true;
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
